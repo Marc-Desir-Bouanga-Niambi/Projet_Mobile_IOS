e76224dec4895ce35426952fb5b96a25d5d07329
Keliane Kossa <christm.3i7@gmail.com>
2026-02-06 11:49:54 +0100

    Tests

--- SwiftFirstAppTests/SwiftFirstAppTests.swift
+++ SwiftFirstAppTests/SwiftFirstAppTests.swift
@@ -1,17 +1,72 @@
-//
-//  SwiftFirstAppTests.swift
-//  SwiftFirstAppTests
-//
-//  Created by Keliane Serane Moayé Kossa on 04/02/2026.
-//
-
 import Testing
 @testable import SwiftFirstApp
 
 struct SwiftFirstAppTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test
+    func testRegisterUser() {
+        // GIVEN
+        PersistenceService.deleteUser()
+        let authVM = AuthViewModel()
+
+        // WHEN
+        authVM.register(
+            name: "Alice",
+            email: "alice@test.com",
+            password: "1234"
+        )
+
+        // THEN
+        #expect(authVM.isAuthenticated == true)
+        #expect(authVM.currentUser != nil)
+        #expect(authVM.currentUser?.email == "alice@test.com")
     }
 
+    @Test
+    func testLoginUser() {
+        // GIVEN
+        PersistenceService.deleteUser()
+
+        let authVM = AuthViewModel()
+        authVM.register(
+            name: "Bob",
+            email: "bob@test.com",
+            password: "abcd"
+        )
+
+        // Nouvelle instance = simulation redémarrage app
+        let authVM2 = AuthViewModel()
+
+        // WHEN
+        authVM2.login(email: "bob@test.com", password: "abcd")
+
+        // THEN
+        #expect(authVM2.isAuthenticated == true)
+        #expect(authVM2.currentUser?.email == "bob@test.com")
+    }
+
+    @Test
+    func testUpdateUser() {
+        // GIVEN
+        PersistenceService.deleteUser()
+
+        let authVM = AuthViewModel()
+        authVM.register(
+            name: "Charlie",
+            email: "charlie@test.com",
+            password: "pass123"
+        )
+
+        // WHEN
+        authVM.updateUser(
+            name: "Charlie Updated",
+            email: "charlie2@test.com",
+            password: "newpass"
+        )
+
+        // THEN
+        #expect(authVM.currentUser?.name == "Charlie Updated")
+        #expect(authVM.currentUser?.email == "charlie2@test.com")
+        #expect(authVM.currentUser?.password == "newpass")
+    }
 }
