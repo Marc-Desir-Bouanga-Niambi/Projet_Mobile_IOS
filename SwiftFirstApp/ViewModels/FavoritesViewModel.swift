d4be8223da03b388180a5b487613ff17ae577933
Clement61646F <toublancclement@gmail.com>
2026-02-06 10:17:51 +0100

    Changement pour voir ce qui utilise l'API pour l'affichage car je comprend pas

--- SwiftFirstApp/ViewModels/FavoritesViewModel.swift
+++ SwiftFirstApp/ViewModels/FavoritesViewModel.swift
@@ -22,45 +22,46 @@ final class FavoritesViewModel: ObservableObject {
 
         // Ici on récupère les films à partir des IDs stockés dans user.favoriteMovieIds
         
-        movieService.fetchMovies { [weak self] result in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let allMovies):
-                    // On filtre uniquement les films présents dans favoriteMovieIds
-                    self?.favoriteMovies = allMovies.filter { user.favoriteMovieIds.contains($0.id) }
-                case .failure(let error):
-                    self?.errorMessage = error.localizedDescription
-                }
-            }
-        }
+//        movieService.fetchMovies { [weak self] result in
+//            DispatchQueue.main.async {
+//                switch result {
+//                case .success(let allMovies):
+//                    // On filtre uniquement les films présents dans favoriteMovieIds
+//                    self?.favoriteMovies = allMovies.filter { user.favoriteMovieIds.contains($0.id) }
+//                case .failure(let error):
+//                    self?.errorMessage = error.localizedDescription
+//                }
+//            }
+//        }
     }
 
     // MARK: - Ajouter un film aux favoris
     func addToFavorites(movie: Movie) {
         guard var user = authVM.currentUser else { return }
 
-        if !user.favoriteMovieIds.contains(movie.id) {
-            user.favoriteMovieIds.append(movie.id)
-            authVM.currentUser = user                  // mettre à jour l’utilisateur courant
-            PersistenceService.saveUser(user)          // persistance
-            loadFavorites()                             // mettre à jour la liste
-        }
+//        if !user.favoriteMovies.contains(movie.id) {
+//            user.favoriteMovies.append(movie.id)
+//            authVM.currentUser = user                  // mettre à jour l’utilisateur courant
+//            PersistenceService.saveUser(user)          // persistance
+//            loadFavorites()                             // mettre à jour la liste
+//        }
     }
 
     // MARK: - Retirer un film des favoris
     func removeFromFavorites(movie: Movie) {
         guard var user = authVM.currentUser else { return }
 
-        if let index = user.favoriteMovieIds.firstIndex(of: movie.id) {
-            user.favoriteMovieIds.remove(at: index)
-            authVM.currentUser = user
-            PersistenceService.saveUser(user)
-            loadFavorites()
-        }
+//        if let index = user.favoriteMovies.firstIndex(of: movie.id) {
+//            user.favoriteMovies.remove(at: index)
+//            authVM.currentUser = user
+//            PersistenceService.saveUser(user)
+//            loadFavorites()
+//        }
     }
 
     // MARK: - Vérifier si un film est favori
     func isFavorite(_ movie: Movie) -> Bool {
-        return authVM.currentUser?.favoriteMovieIds.contains(movie.id) ?? false
+//        return authVM.currentUser?.favoriteMovies.contains(movie.id) ?? false
+        return true
     }
 }
